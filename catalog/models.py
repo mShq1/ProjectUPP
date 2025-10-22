db293e8dfe858436591d1dfd1d70767b817da9c4
KawaiiZhele <e.d.zhiltsova@gmail.com>
2025-10-22 13:03:54 +0300

    modified: added new files and fixed old bugs

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User  # для связи с пользователем
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +9,20 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(CartItem)
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
