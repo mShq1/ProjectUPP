a0541abd38871b43ab12cbfd22ddd8590c9d850d
KawaiiZhele <e.d.zhiltsova@gmail.com>
2025-11-25 12:35:59 +0300

    modified: admin panel added

--- catalog/models.py
+++ catalog/models.py
@@ -1,28 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User  # для связи с пользователем
 
-class Product(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+
+
+    def __str__(self):
+        return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return f"{self.product.name} image"
+
 class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
-    def __str__(self):
-        return f"{self.quantity} x {self.product.name}"
-
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     items = models.ManyToManyField(CartItem)
     created_at = models.DateTimeField(auto_now_add=True)
-    completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Order {self.id} by {self.user.username}"
