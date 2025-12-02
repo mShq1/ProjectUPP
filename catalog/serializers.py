917f1203ccd92be997141ef9fe2c91875676ec08
KawaiiZhele <73380016+KawaiiZhele@users.noreply.github.com>
2025-12-03 01:04:35 +0300

    Develop true (#4)
    * added: эндпойнты окончательные и авторизация с токенами
    
    * added: эндпойнты окончательные и авторизация с токенами

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,62 +1,41 @@
 from rest_framework import serializers
-from .models import Category, Product, ProductImage, ProductVariant, Cart, CartItem
-from django.contrib.auth.models import User
-
-class CategorySerializer(serializers.ModelSerializer):
-    children = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'slug', 'parent', 'children']
-
-    def get_children(self, obj):
-        return CategorySerializer(obj.children.all(), many=True).data
-
-
-class ProductImageSerializer(serializers.ModelSerializer):
-    image_url = serializers.SerializerMethodField()
-    
-    class Meta:
-        model = ProductImage
-        fields = ['id', 'image', 'image_url']
-    
-    def get_image_url(self, obj):
-        request = self.context.get('request')
-        if obj.image:
-            if request:
-                return request.build_absolute_uri(obj.image.url)
-            return obj.image.url
-        return None
-
-
-class ProductVariantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductVariant
-        fields = ['id', 'size', 'color']
+from .models import Product, CartItem, Order
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True, read_only=True)
-    variants = ProductVariantSerializer(many=True, read_only=True)
-    category = CategorySerializer(read_only=True)
-
     class Meta:
         model = Product
-        fields = ['id', 'category', 'name', 'description', 'price', 'is_available', 'images', 'variants']
+        fields = '__all__'
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer(read_only=True)
-    variant = ProductVariantSerializer(read_only=True)
 
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'variant', 'quantity']
+        fields = '__all__'
+        read_only_fields = ['order']  #если связь есть
 
 
-class CartSerializer(serializers.ModelSerializer):
+class OrderItemWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(many=True, read_only=True)
+    items_data = OrderItemWriteSerializer(many=True, write_only=True)
 
     class Meta:
-        model = Cart
-        fields = ['id', 'user', 'items']
+        model = Order
+        fields = '__all__'
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items_data')
+        order = Order.objects.create(**validated_data)
+
+        for item in items_data:
+            CartItem.objects.create(order=order, **item)
+
+        return order
---
+++ catalog/urls.py
@@ -0,0 +1,49 @@
+from django.urls import path
+from catalog.views import RegisterView, CustomAuthToken
+from .views import (
+    ProductListView,
+    ProductDetailView,
+    CartView,
+    AddToCartView,
+    RemoveFromCartView,
+    ClearCartView,
+    CreateOrderView,
+    OrderListView,
+    OrderDetailView,
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
+    # Товары
+    path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+
+    # Корзина
+    path('cart/', CartView.as_view(), name='cart-detail'),
+    path('cart/add/', AddToCartView.as_view(), name='cart-add'),
+    path('cart/remove/', RemoveFromCartView.as_view(), name='cart-remove'),
+    path('cart/clear/', ClearCartView.as_view(), name='cart-clear'),
+
+    # Заказы
+    path('orders/', OrderListView.as_view(), name='order-list'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
+    path('orders/create/', CreateOrderView.as_view(), name='order-create'),
+]
+
+# Как это работает:
+# Продукты
+# /products/ — GET список всех товаров
+# /products/{id}/ — GET конкретный товар
+
+# Корзина
+# /cart/ — GET корзина текущего пользователя
+# /cart/add/ — POST добавить товар
+# /cart/remove/ — POST удалить товар
+# /cart/clear/ — POST очистить корзину
+
+# Заказы
+# /orders/ — GET список заказов пользователя
+# /orders/{id}/ — GET детали заказа
+# /orders/create/ — POST оформить заказ из корзины
+
