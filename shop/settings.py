d8f66e865473ff9b11722522c9bbdca29bba6b58
mShq1 <mashastepa03@gmail.com>
2025-12-03 23:42:50 +0300

    картинки отображаются

--- shop/settings.py
+++ shop/settings.py
@@ -132,5 +132,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/product_images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'product_images')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- shop/urls.py
+++ shop/urls.py
@@ -9,3 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('catalog.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
